eabc6298bf438891ee7496eea4d65bddcd2963bc
Vinicius F, Maycon D, Igor A <viniciusvfs@hotmail.com>
2016-11-09 09:45:05 -0200

    Primeiro Commit - cenas criadas

---
+++ Assets/Scripts/InicioUIController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InicioUIController : MonoBehaviour {
+
+	public void Jogar()
+    {
+        SceneManager.LoadScene("partida");
+    }
+
+    public void JPrologo()
+    {
+        SceneManager.LoadScene("prologo");
+    }
+}
