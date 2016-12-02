c9faf70000356437317c013fbfa22b6c6ab817a4
Maycon D <02081410656@est.udicentro.local>
2016-12-02 18:43:16 -0200

    Programando a movimentãcao basica das pecas

---
+++ Assets/Scripts/Movimentacao.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimentacao : MonoBehaviour {
+
+    GameController gameController;
+
+    bool colidindo;
+    GameObject casa;
+
+    int chaoMask;
+
+    void Awake()
+    {
+        gameController = FindObjectOfType(typeof(GameController)) as GameController;
+        colidindo = false;
+        casa = null;
+
+        chaoMask = LayerMask.GetMask("Chao");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Casas"))
+        {
+            colidindo = true;
+            casa = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (colidindo && other.gameObject == casa)
+        {
+            colidindo = false;
+            casa = null;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        gameController.processarMouseDown(transform.gameObject, casa);
+    }
+
+    void OnMouseUp()
+    {
+        gameController.processarMouseUp(transform.gameObject, casa);
+    }
+
+    void OnMouseDrag()
+    {
+        if (gameController.estado == Estado.Arrastando && gameController.pecaEscolhida == transform.gameObject)
+        {
+            Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(raio,out hit, 300f, chaoMask))
+            {
+                transform.position = hit.point;
+            }
+        }
+    }
+}
