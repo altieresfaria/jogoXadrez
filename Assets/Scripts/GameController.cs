e42a300b898a0b72598a524e49509e7f907c6b28
Maycon D <02081410656@est.udicentro.local>
2016-12-02 21:53:29 -0200

    Acrescentando as demais peca

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,14 @@ using UnityEngine.UI;
     public GameObject reiPreto = null;
     public GameObject torreBranca = null;
     public GameObject torrePreta = null;
+    public GameObject damaBranca = null;
+    public GameObject damaPreta = null;
+    public GameObject peaoBranco = null;
+    public GameObject peaoPreto = null;
+    public GameObject bispoBranco = null;
+    public GameObject bispoPreto = null;
+    public GameObject cavaloBranco = null;
+    public GameObject cavaloPreto = null;
 
     public Text txtMsg = null;
     public Text txtXeque = null;
@@ -37,13 +45,40 @@ using UnityEngine.UI;
         txtXeque.text = "";
         informarAguardando();
 
-        Util.instanciarRei('e', 1, Cor.Branca, partida, reiBranco);
         Util.instanciarTorre('a', 1, Cor.Branca, partida, torreBranca);
+        Util.instanciarCavalo('b', 1, Cor.Branca, partida, cavaloBranco);
+        Util.instanciarBispo('c', 1, Cor.Branca, partida, bispoBranco);
+        Util.instanciarDama('d', 1, Cor.Branca, partida, damaBranca);
+        Util.instanciarRei('e', 1, Cor.Branca, partida, reiBranco);
+        Util.instanciarBispo('f', 1, Cor.Branca, partida, bispoBranco);
+        Util.instanciarCavalo('g', 1, Cor.Branca, partida, cavaloBranco);
         Util.instanciarTorre('h', 1, Cor.Branca, partida, torreBranca);
+        Util.instanciarPeao('a', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('b', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('c', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('d', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('e', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('f', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('g', 2, Cor.Branca, partida, peaoBranco);
+        Util.instanciarPeao('h', 2, Cor.Branca, partida, peaoBranco);
 
-        Util.instanciarRei('a', 8, Cor.Preta, partida, torrePreta);
+
+        Util.instanciarTorre('a', 8, Cor.Preta, partida, torrePreta);
+        Util.instanciarCavalo('b', 8, Cor.Preta, partida, cavaloPreto);
+        Util.instanciarBispo('c', 8, Cor.Preta, partida, bispoPreto);
+        Util.instanciarDama('d', 8, Cor.Preta, partida, damaPreta);
         Util.instanciarRei('e', 8, Cor.Preta, partida, reiPreto);
+        Util.instanciarBispo('f', 8, Cor.Preta, partida, bispoPreto);
+        Util.instanciarCavalo('g', 8, Cor.Preta, partida, cavaloPreto);
         Util.instanciarTorre('h', 8, Cor.Preta, partida, torrePreta);
+        Util.instanciarPeao('a', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('b', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('c', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('d', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('e', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('f', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('g', 7, Cor.Preta, partida, peaoPreto);
+        Util.instanciarPeao('h', 7, Cor.Preta, partida, peaoPreto);
 
 
     }
