ce5b8fb052ea91b3362d9063c7752d0d6bc02a42
Maycon D <02081410656@est.udicentro.local>
2016-12-02 16:57:21 -0200

    ReiPreto

---
+++ Assets/Scripts/GameController2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using tabuleiro;
+using xadrez;
+
+class GameController2 : MonoBehaviour
+{
+
+    public GameObject reiPreto = null;
+
+    PartidaDeXadrez partida;
+
+    void Start()
+    {
+        partida = new PartidaDeXadrez();
+        Util.instanciarRei('e', 8, Cor.Branca, partida, reiPreto);
+    }
+}
