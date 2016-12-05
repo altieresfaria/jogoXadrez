952f290923a1d064d34a4ca303d1fd44b917ab2c
MAYCON DOUGLAS DA SILVA <02081410656@est.udicentro.local>
2016-12-05 11:33:48 -0200

    Construindo a cena de prologo

--- Assets/Scripts/PrologoUIController.cs
+++ Assets/Scripts/PrologoUIController.cs
@@ -1,8 +1,56 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class PrologoUIController : MonoBehaviour
 {
+
+    int PaginaAtual;
+    int N;
+    List<GameObject> paginas;
+
+    void Start()
+    {
+        PaginaAtual = 0;
+        N = 1;
+        paginas = new List<GameObject>();
+
+        GameObject obj = GameObject.Find("pag0");
+        paginas.Add(obj);
+        obj = GameObject.Find("pag" + N);
+        while (obj != null)
+        {
+            paginas.Add(obj);
+            obj.SetActive(false);
+            N++;
+            obj = GameObject.Find("pag" + N);
+
+        }
+
+    }
+
+    int proxima (int i)
+    {
+        return (i == N - 1) ? 0 : i + 1;
+        }
+        
+    int anterior (int i){
+
+        return (i == 0) ?  N - 1 : i - 1;
+    }
+
+    public void avancarPagina()
+    {
+        paginas[PaginaAtual].SetActive(false);
+        PaginaAtual = proxima(PaginaAtual);
+        paginas[PaginaAtual].SetActive(true); 
+    }
+    public void voltarPagina()
+    {
+        paginas[PaginaAtual].SetActive(false);
+        PaginaAtual = anterior(PaginaAtual);
+        paginas[PaginaAtual].SetActive(true);
+    }
 
     public void Sair()
     {
